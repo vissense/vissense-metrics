9485de69894dd2ff430a47ebb4a4fbf864e474b2
theborakompanioni <theborakompanioni+github@gmail.com>
2015-03-03 19:26:40 +0100

    run chrome on travis with --no-sandbox flag

--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,81 @@
-module.exports = function(config) {
-    'use strict';
+module.exports = function (config) {
+  'use strict';
 
-    config.set({
+  var configuration = {
 
-        // base path, that will be used to resolve files and exclude
-        basePath: './',
+    // base path, that will be used to resolve files and exclude
+    basePath: './',
 
-        frameworks: ['jasmine'],
+    frameworks: ['jasmine'],
 
-        files: [
-            'bower_components/jquery/dist/jquery.min.js',
-            'bower_components/lodash/dist/lodash.min.js',
-            'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
+    files: [
+      'bower_components/jquery/dist/jquery.min.js',
+      'bower_components/lodash/dist/lodash.min.js',
+      'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
 
-            //'bower_components/brwsrfy-metrics/dist/brwsrfy-metrics.js',
-            'bower_components/countonmejs/src/main/countonmejs.js',
+      'bower_components/countonmejs/src/main/countonmejs.js',
+      'bower_components/vissense/dist/vissense.min.js',
 
-            'bower_components/vissense/dist/vissense.min.js',
+      'src/main/metrics/vissense.metrics.js',
 
-            'src/main/metrics/vissense.metrics.js',
+      'spec/**/*.js',
+      // fixtures
+      {pattern: 'spec/**/*.html', watched: true, served: true, included: false}
+    ],
 
-            'spec/**/*.js',
-            // fixtures
-            {pattern: 'spec/**/*.html', watched: true, served: true, included: false}
-        ],
+    exclude: [],
 
-        exclude: [
-        ],
+    reporters: ['progress', 'coverage'],
 
-        reporters: ['progress', 'coverage'],
+    port: 3000,
 
-        port: 3000,
+    colors: true,
 
-        colors: true,
+    logLevel: config.LOG_INFO,
 
-        logLevel: config.LOG_INFO,
+    autoWatch: true,
 
-        autoWatch: true,
+    preprocessors: {
+      'src/main/**/*.js': ['coverage']
+    },
 
-        preprocessors: {
-          'src/main/**/*.js': ['coverage']
-        },
+    browsers: ['PhantomJS', 'Chrome', 'Firefox'],
+    //browsers: ['PhantomJS'],
 
-        browsers: ['PhantomJS', 'Chrome'],
-        //browsers: ['PhantomJS'],
+    customLaunchers: {
+      Chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security']
+      },
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
-        customLaunchers: {
-          Chrome_without_security: {
-            base: 'Chrome',
-            flags: ['--disable-web-security']
-          }
-        },
+    coverageReporter: {
+      reporters: [
+        {type: 'lcov', dir: 'bin/coverage/'}
+      ]
+    },
 
-        coverageReporter: {
-            reporters:[
-              {type: 'lcov', dir:'bin/coverage/'}
-            ]
-        },
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  };
 
-        // if true, it capture browsers, run tests and exit
-        singleRun: true
-    });
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['PhantomJS', 'Firefox', 'Chrome_travis_ci'];
+  }
+
+  if (process.platform === 'win32') {
+    // @link https://github.com/karma-runner/karma-phantomjs-launcher/issues/27
+    // @link https://github.com/karma-runner/karma/issues/931
+    configuration.browsers.splice(configuration.browsers.indexOf('PhantomJS'), 1);
+    configuration.browsers.push('IE');
+  }
+
+  config.set(configuration);
 };
